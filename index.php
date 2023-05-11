3f734962ad6873f6d2f1777594341b24b3b32f5c
zeninlz <131748598+zeninlz@users.noreply.github.com>
2023-05-11 14:33:31 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,27 @@
+<?php
+$Getal1=12;
+$Getal2 =10;
+
+$resultaat = $Getal1 + $Getal2;
+echo  $resultaat;
+?>
+
+<?php
+$Getal1=12;
+$Getal2 =10;
+
+$resultaat = $Getal1 * $Getal2;
+echo  $resultaat;
+?>
+
+<?php
+$MyName = "martino";
+echo "ewa ".$MyName. " !"
+?>
+
+
+<?php
+$mijnNaam = "Carl";
+$jouwNaam = "camel";
+echo $mijnNaam."zit bij ".$jouwNaam."in de klas!";
+?>
