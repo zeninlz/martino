0883b028556d0efafb0db347d2e82bf11b4b95e5
zeninlz <131748598+zeninlz@users.noreply.github.com>
2023-09-23 10:28:43 +0200

    Add files via upload

---
+++ web.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+
+
+
+Route::get('/', function () {
+    return [
+        "Uranus",
+        "Jupiter",
+        "Mars",
+        "Aarde",
+        "Saturnus",
+        "Pluto",
+        "Neptunus",
+        "Venus"
+    ];
+});
+ //Route::Post ('/register',[UserController::class, 'register']);
+//Route::post ('/logout', [UserController::class, 'logout']);
