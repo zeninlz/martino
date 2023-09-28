4e7b15988d5b8b051d9d56b20e5fdd386f24b4f1
zeninlz <131748598+zeninlz@users.noreply.github.com>
2023-09-28 13:43:45 +0200

    Add files via upload

--- web.php
+++ web.php
@@ -16,20 +16,28 @@ use App\Http\Controllers\UserController;
 
 
 
-// dit is de opdracht van 30 september 
+
 Route::get('/', function () {
-    return [
-        "Uranus",
-        "Jupiter",
-        "Mars",
-        "Aarde",
-        "Saturnus",
-        "Pluto",
-        "Neptunus",
-        "Venus"
+    $planets = [
+        [
+            'name' => 'Mars',
+            'description' => 'Mars is the fourth planet from the Sun and the second-smallest planet in the Solar System, being larger than only Mercury.'
+        ],
+        [
+            'name' => 'Venus',
+            'description' => 'Venus is the second planet from the Sun. It is named after the Roman goddess of love and beauty.'
+        ],
+        [
+            'name' => 'Earth',
+            'description' => 'Our home planet is the third planet from the Sun, and the only place we know of so far thats inhabited by living things.'
+        ],
+        [
+            'name' => 'Jupiter',
+            'description' => 'Jupiter is a gas giant and doesn\'t have a solid surface, but it may have a solid inner core about the size of Earth.'
+        ],
     ];
+    
+    return view('welcome',['planets' => $planets]);
 });
-
-
  //Route::Post ('/register',[UserController::class, 'register']);
 //Route::post ('/logout', [UserController::class, 'logout']);
